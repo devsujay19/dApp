abf7dc22575d5cfb819c902ec060b4ddb7760ad9
Sujay Mukherjee <sujay.mukherjeedev@gmail.com>
2024-05-26 17:32:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My first dApp
+
+My first Decentralized Application made with Solidity and Hardhat for the Backend and ReactJS (Vite Server) for the Frontend.
---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => (
+  <div className="min-h-screen text-white flex justify-center items-center">
+    <span className="cursor-pointer" onClick={() => {
+      window.location.href = "https://github.com/devsujay19/dApp";
+    }}>My first Decentralized Application!</span>
+  </div>
+);
+
+export default App;
